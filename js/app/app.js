0c001061a044ee1fa525757d6874adff29960fdf
lshapz <laurashapiro01@gmail.com>
2017-05-08 17:31:34 -0400

    why

---
+++ js/app/app.js
@@ -0,0 +1,30 @@
+angular
+    .module('app', [
+      'ui.router',
+      'ngSanitize'
+    ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('top', {
+        url: '/top',
+        templateUrl: 'views/top-stories.html',
+        controller: 'TopStoriesController as top',
+        resolve: {
+          posts: function (PostsService) {
+            return PostsService.getTopStories();
+          }
+        }
+      })
+      .state('post', {
+        url: '/post/:id',
+        templateUrl: 'views/post.html',
+        controller: 'PostController as post',
+        resolve: {
+          post: function ($stateParams, PostsService) {
+            return PostsService.getPost($stateParams.id);
+          }
+        }
+      });
+    
+    $urlRouterProvider.otherwise('/top');
+  });
